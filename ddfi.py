357cc5ad8f5f09c68f755225d45388d5abc5be6e
Mr-Z-2697 <74594146+Mr-Z-2697@users.noreply.github.com>
2020-11-17 23:49:14 +0800

    add more arguments

--- ddfi.py
+++ ddfi.py
@@ -5,15 +5,22 @@ import subprocess
 class args:
     pass
 parser = argparse.ArgumentParser(description='animation auto duplicated frame remove and frame interpolate tool',formatter_class=argparse.RawTextHelpFormatter)
-parser.add_argument('-i','--input',required=True,type=str,help='source file, any format ffmpeg can decode')
-parser.add_argument('-o','--output',required=False,type=str,help='output file, default \"input file\"_interp.mkv')
-parser.add_argument('-tf','--temp_folder',required=False,type=str,help='temp folder, default \"output file\"_tmp\\')
-parser.add_argument('-st','--start_time',required=False,type=str,help='cut input video from this time, format h:mm:ss.nnn')
-parser.add_argument('-et','--end_time',required=False,type=str,help='cut input video end to this time, format h:mm:ss.nnn')
-parser.add_argument('-as','--audio_stream',required=True,type=str,help='set audio stream index, starts from 0, \"no\" means don\'t output audio')
-parser.add_argument('-q','--output_crf',required=False,type=int,help='output video crf value, default 18')
-parser.add_argument('-qi','--intermedia_crf',required=False,type=int,help='intermedia video crf value, default 12')
-parser.add_argument('-if','--interpolation_factor',required=False,type=int,help='interpolation factor, default 8, not recommend to decrease it')
+parser.add_argument('-i','--input',required=True,type=str,help='source file, any format ffmpeg can decode\n ')
+parser.add_argument('-o','--output',required=False,type=str,help='output file, default \"input file\"_interp.mkv\n ')
+parser.add_argument('-tf','--temp_folder',required=False,type=str,help='temp folder, default \"output file\"_tmp\\\n ')
+parser.add_argument('-st','--start_time',required=False,type=str,help='cut input video from this time, format h:mm:ss.nnn\n ')
+parser.add_argument('-et','--end_time',required=False,type=str,help='cut input video end to this time, format h:mm:ss.nnn\n ')
+parser.add_argument('-as','--audio_stream',required=True,type=int,help='set audio stream index, starts from 0, \"no\" means don\'t output audio\n ')
+parser.add_argument('-q','--output_crf',required=False,type=int,help='output video crf value, interger. if a codec don\'t has -crf option is used, \n--ffmpeg_params_output can be used as a workaround. default 18\n ')
+parser.add_argument('-qi','--intermedia_crf',required=False,type=int,help='intermedia video crf value, interger. default 12\n ')
+parser.add_argument('-if','--interpolation_factor',required=False,type=int,help='interpolation factor, interger. default 8, \nnot recommend to decrease it\n ')
+parser.add_argument('-tr','--target_framerate',required=False,type=str,help='target framerate, only affects final output, \ncan be fraction, interger or decimal. default 60000/1001\n ')
+parser.add_argument('-vc','--video_codec',required=False,type=str,help='output video codec, use the name in ffmpeg, \nwill be constrained by output format. default libx264\n ')
+parser.add_argument('-ac','--audio_codec',required=False,type=str,help='output audio codec, similar to -vc. default aac\n ')
+parser.add_argument('-al','--audio_channel_layout',required=False,type=str,help='output audio channel layout, \nuse the name in ffmpeg. default stereo\n ')
+parser.add_argument('-ab','--audio_bitrate',required=False,type=str,help='output audio bitrate, use it like in ffmpeg. default 256k\n ')
+parser.add_argument('-cd','--consecutive_drop_max',required=False,type=int,help='set the maximum number of consecutive dropped frames. \ndefault 2. this will be passed to the \"max\" option of ffmpeg filter mpdecimate.\n ')
+parser.add_argument('--ffmpeg_params_output',required=False,type=str,help='other ffmpeg parameters used in final step, \nuse it carefully. default \"\" (empty)\n ')
 parser.parse_args(sys.argv[1:],args)
 
 inFile=args.input
@@ -28,6 +35,13 @@ ffau2='' if args.audio_stream == 'no' else f'-map 1:a:0'
 crfo=18 if args.output_crf is None else args.output_crf
 crfi=12 if args.intermedia_crf is None else args.intermedia_crf
 xinterp=8 if args.interpolation_factor is None else args.interpolation_factor
+fro='60000/1001' if args.target_framerate is None else args.target_framerate
+codecov='-c:v libx264' if args.video_codec is None else f'-c:v {args.video_codec}'
+codecoa='-c:a aac' if args.audio_codec is None else f'-c:a {args.audio_codec}'
+clo='-channel_layout stereo' if args.audio_channel_layout is None else f'-channel_layout {args.audio_channel_layout}'
+abo='-b:a 256k' if args.audio_bitrate is None else f'-b:a {args.audio_bitrate}'
+ffparamo='' if args.ffmpeg_params_output is None else args.ffmpeg_params_output
+mpdmax=2 if args.consecutive_drop_max is None else args.consecutive_drop_max
 
 tmpDedup=os.path.abspath(tmpFolder+'dedup_done.mkv')
 tmpInterpXn=f'{tmpFolder}interpX{xinterp}_done.264'
@@ -55,7 +69,7 @@ else:
     os.mkdir(tmpFolder)
 
 if not os.path.exists(tmpDedup):
-    ff_mpdecimate=f'\"{ffpath}ffmpeg.exe\" {ffss} {ffto} -i \"{inFile}\" -vf mpdecimate=max=2 -crf {crfi} -preset 1 -pix_fmt yuv420p -sample_fmt s16 -c:v libx264 -c:a flac -map v:0 {ffau} \"{tmpFolder}dedup.mkv\" -y'
+    ff_mpdecimate=f'\"{ffpath}ffmpeg.exe\" {ffss} {ffto} -i \"{inFile}\" -vf mpdecimate=max={mpdmax} -crf {crfi} -preset 1 -pix_fmt yuv420p -sample_fmt s16 -c:v libx264 -c:a flac -map v:0 {clo} {ffau} \"{tmpFolder}dedup.mkv\" -y'
     print(ff_mpdecimate)
     subprocess.run(ff_mpdecimate,shell=True)
     os.rename(f'{tmpFolder}dedup.mkv',tmpDedup)
@@ -114,6 +128,6 @@ if not os.path.exists(f'{tmpInterpXn}.mkv'):
     print(mmg)
     subprocess.run(mmg,shell=True)
 
-ff_down60=f'\"{ffpath}ffmpeg.exe\" -i \"{tmpInterpXn}.mkv\" -i {tmpDedup} -map 0:v:0 {ffau2} -vf framerate=60000/1001 -crf {crfo} -pix_fmt yuv420p -c:v libx264 -c:a aac -b:a 256k \"{outFile}\" -y'
+ff_down60=f'\"{ffpath}ffmpeg.exe\" -i \"{tmpInterpXn}.mkv\" -i {tmpDedup} -map 0:v:0 {ffau2} -vf framerate={fro} -crf {crfo} {codecov} {codecoa} {abo} {ffparamo} \"{outFile}\" -y'
 print(ff_down60)
 subprocess.run(ff_down60,shell=True)
